387fae7158a4d5da1fcb565dfa64028cf60996f1
Patryk Zwolak <p.zwolak.93@gmail.com>
2019-12-31 19:47:13 +0100

    Is enougt for today

--- src/App.js
+++ src/App.js
@@ -5,13 +5,28 @@ import Header from "./sections/Header/Header";
 import "./styles/App.scss";
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <ParallaxBackground />
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  state = {};
+  componentDidMount() {
+    new fullpage("#fullpage", {
+      //options
+      sectionsColor: ["#f2f2f2", "#4BBFC3", "#7BAABE", "whitesmoke", "#000"]
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <ParallaxBackground />
+        <Header />
+        <div id="fullpage">
+          <div class="section">Some section</div>
+          <div class="section">Some section</div>
+          <div class="section">Some section</div>
+          <div class="section">Some section</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
