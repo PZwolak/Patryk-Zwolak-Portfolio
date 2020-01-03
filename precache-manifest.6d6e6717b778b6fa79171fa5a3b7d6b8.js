46fed56ff3e253cbb0dd3cf8c7d88cb5d6e659b8
Patryk Zwolak <p.zwolak.93@gmail.com>
2020-01-03 18:44:51 +0100

    Updates

---
+++ precache-manifest.6d6e6717b778b6fa79171fa5a3b7d6b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6def04592fc08de937e8ed15ddd188a5",
+    "url": "/patryk-zwolak-portfolio/index.html"
+  },
+  {
+    "revision": "c3926f0e643507e99177",
+    "url": "/patryk-zwolak-portfolio/static/css/2.2831ab12.chunk.css"
+  },
+  {
+    "revision": "3e2f525431fe8007e10c",
+    "url": "/patryk-zwolak-portfolio/static/css/main.c52b1071.chunk.css"
+  },
+  {
+    "revision": "c3926f0e643507e99177",
+    "url": "/patryk-zwolak-portfolio/static/js/2.50a93613.chunk.js"
+  },
+  {
+    "revision": "4c1666def774ef743803dc3de259e5c5",
+    "url": "/patryk-zwolak-portfolio/static/js/2.50a93613.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e2f525431fe8007e10c",
+    "url": "/patryk-zwolak-portfolio/static/js/main.8ca2d564.chunk.js"
+  },
+  {
+    "revision": "9129f05d18b574ff6623",
+    "url": "/patryk-zwolak-portfolio/static/js/runtime-main.fef8abca.js"
+  }
+]);
